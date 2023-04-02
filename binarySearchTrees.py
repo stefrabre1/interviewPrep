59c0b4fd3262a87f5c373edb0c5ab2d040f9600a
Stephen Personal <stefrabre@gmail.com>
2023-04-02 16:47:07 -0700

    started writing out BST structure and insert

--- binarySearchTrees.py
+++ binarySearchTrees.py
@@ -1,1 +1,51 @@
 # BST practice
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+        
+class binarySearchTree:
+    def __init__(self):
+        self.top = None
+        
+    def insert(self, node):
+        # empty tree
+        if self.top is None:
+            print("Yeah")
+            self.top = node
+            return
+        
+        temp = self.top
+        
+        while True: # lol
+            if temp.data == node.data:
+                raise Exception("Duplicate data found!")
+
+            # left child 
+            while temp.left is not None and node.data < temp.data:
+                temp = temp.left
+                
+            # right child
+            while temp.right is not None and node.data > temp.data:
+                temp = temp.left
+                
+            # insertion
+            if node.data < temp.data:
+                temp.left = node
+                node.parent = temp
+                return
+            elif node.data > temp.data:
+                temp.right = node
+                node.parent = temp
+                return
+                
+## main 
+
+bst = binarySearchTree()
+bst.insert(Node(5))
+bst.insert(Node(3))
+bst.insert(Node(7))
+
